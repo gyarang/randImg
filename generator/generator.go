8107ef107891f4d586f44a1f51a1d72ef896a411
gyarang <qlqlqkqlqn@gmail.com>
2021-11-29 16:49:23 +0900

    Move generate type enum
    왜 거깃서

--- generator/generator.go
+++ generator/generator.go
@@ -1,6 +1,12 @@
 package generator
 
 type GenerateType int
+
+const (
+	IMAGE GenerateType = 1 + iota
+	VIDEO
+)
+
 type generator interface {
 	Generate() error
 }
--- generator/generatorFactory.go
+++ generator/generatorFactory.go
@@ -1,10 +1,5 @@
 package generator
 
-const (
-	IMAGE = 0
-	VIDEO = 1
-)
-
 func NewGenerator(g GenerateType, w, h int, p, n string) generator {
 	switch g {
 	case IMAGE:
