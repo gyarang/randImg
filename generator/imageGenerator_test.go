9222d1bebc4ae19097ecaf7ef86351b77362b878
gyarang <qlqlqkqlqn@gmail.com>
2021-11-29 16:50:54 +0900

    Add benchmark
    Add benchmark

--- generator/imageGenerator_test.go
+++ generator/imageGenerator_test.go
@@ -7,3 +7,28 @@ import (
 func TestGetRandomColor(t *testing.T) {
 	getRandomColor()
 }
+
+func BenchmarkGetRandomColor(b *testing.B) {
+	getRandomColor()
+}
+
+func BenchmarkNewImageGeneratorFHD(b *testing.B) {
+	newImageGenerator(1920, 1080, "./", "asd")
+}
+
+func BenchmarkNewImageGenerator100(b *testing.B) {
+	newImageGenerator(100, 100, "./", "asd")
+}
+
+func BenchmarkGenerateFHD(b *testing.B) {
+	image := newImageGenerator(1920, 1080, "./", "testFile")
+	b.ResetTimer()
+	image.Generate()
+}
+
+func BenchmarkGenerate100(b *testing.B) {
+	image := newImageGenerator(100, 100, "./", "testFile")
+	b.ResetTimer()
+	image.Generate()
+}
+
